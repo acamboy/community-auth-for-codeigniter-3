b6f62e8f01e9e2104d88b7fa4beb0137d43028b1
Brian Gottier <iamsentme@gmail.com>
2017-01-10 00:13:00 -0800

    My form helper updated for CI 3.1.3

--- application/third_party/community_auth/helpers/MY_form_helper.php
+++ application/third_party/community_auth/helpers/MY_form_helper.php
@@ -31,9 +31,6 @@ if( ! function_exists('form_open') )
 	{
 		$CI =& get_instance();
 
-		// Load URL helper for the site_url and base_url functions
-		$CI->load->helper('url');
-
 		// Set the link protocol to https if secure
 		$link_protocol = ( USE_SSL && is_https() ) 
 			? 'https' 
@@ -42,15 +39,10 @@ if( ! function_exists('form_open') )
 		// If no action is provided then set to the current url
 		if ( ! $action)
 		{
-			$action = current_url($action);
+			$action = $CI->config->site_url($CI->uri->uri_string());
 
-			if( is_https() )
-			{
-				if( parse_url( $action, PHP_URL_SCHEME ) == 'http' )
-				{
-					$action = substr( $action, 0, 4 ) . 's' . substr( $action, 4 );
-				}
-			}
+			if( is_https() && parse_url( $action, PHP_URL_SCHEME ) == 'http' )
+				$action = substr( $action, 0, 4 ) . 's' . substr( $action, 4 );
 
 			$action = ( $_SERVER['QUERY_STRING'] )
 				? $action . '?' . $_SERVER['QUERY_STRING']
@@ -59,7 +51,7 @@ if( ! function_exists('form_open') )
 		// If an action is not a full URL then turn it into one
 		elseif (strpos($action, '://') === FALSE)
 		{
-			$action = site_url($action, $link_protocol);
+			$action = $CI->config->site_url($action);
 		}
 
 		$attributes = _attributes_to_string($attributes);
@@ -77,22 +69,20 @@ if( ! function_exists('form_open') )
 		$form = '<form action="'.$action.'"'.$attributes.">\n";
 
 		// Add CSRF field if enabled, but leave it out for GET requests and requests to external websites
-		if ($CI->config->item('csrf_protection') === TRUE && strpos($action, base_url('', $link_protocol)) !== FALSE && ! stripos($form, 'method="get"'))
+		if ($CI->config->item('csrf_protection') === TRUE && strpos($action, $CI->config->base_url('', $link_protocol)) !== FALSE && ! stripos($form, 'method="get"'))
 		{
 			$hidden[$CI->security->get_csrf_token_name()] = $CI->security->get_csrf_hash();
 		}
 
 		// Add MY CSRF token if MY CSRF library is loaded
-		if( $CI->load->is_loaded('tokens') && strpos($action, base_url('', $link_protocol)) !== FALSE  && ! stripos($form, 'method="get"') )
-		{
+		if( $CI->load->is_loaded('tokens') && strpos($action, $CI->config->base_url('', $link_protocol)) !== FALSE  && ! stripos($form, 'method="get"') )
 			$hidden[ $CI->tokens->name ] = $CI->tokens->token();
-		}
 
 		if (is_array($hidden))
 		{
 			foreach ($hidden as $name => $value)
 			{
-				$form .= '<input type="hidden" name="'.$name.'" value="'.html_escape($value).'" style="display:none;" />'."\n";
+				$form .= '<input type="hidden" name="'.$name.'" value="'.html_escape($value).'" />'."\n";
 			}
 		}
 
